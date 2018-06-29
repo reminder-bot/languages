f57e933b01cb51a552924a567f6554b86ff54bbf
Jude Southworth <judewrs@gmail.com>
2018-06-29 21:38:36 +0100

    Add files via upload

---
+++ strings_EN.py
@@ -0,0 +1,260 @@
+#: english
+
+{
+    'blacklisted' : ''':x: This channel is blacklisted :x:''',
+
+    'admin_required' : 'You need to be an admin to run this command.',
+
+    'help' : '''Please visit https://jellywx.co.uk/help?lang=EN''',
+
+    'help_raw' : [
+        ['''Reminder Commands''', {
+            '$del' : 'delete reminders and intervals on your server. If the reminders are set up for DMs, direct message this command to the bot.',
+
+            '$remind [user/channel] <time-to-reminder> <message>' : 'set up a reminder. Takes times in the format of [num][s/m/h/d], for example 10s for 10 seconds or 2s10m for 2 seconds 10 minutes. An exact time can be provided as `day`/`month`/`year`-`hour`:`minute`:`second`.',
+
+            '$interval [user/channel] <time-to-reminder> <interval> <message>' : 'set up an interval, where the given `message` will be sent every `interval` starting in the given `time-to-reminder`. Takes times in the formats above. Ex. `$interval 0s 20m Hello World!` will send `Hello World!` to your channel every 20 minutes.',
+
+            '$todo' : 'TODO list related commands. Use `$todo help` for more information.',
+
+            '$todos' : 'same as `$todo` but for server-wide task management.',
+
+            '$timezone' : 'set your server\'s timezone, for easier date-based reminders.' }],
+
+        ['''Management Commands''', {
+            '$autoclear [time/s] [channels]' : 'enables/disables autoclearing, where messages sent to the channel (default your channel) will be automatically deleted after time (default 10 seconds).',
+
+            '$clear <user mentions>' : 'clears messages made by a user/s.',
+
+            '$restrict [role mentions]' : 'add/remove roles from being allowed to send channel reminders and intervals.',
+
+            '$tag' : 'Aliasing commands. Use `$tag help` for more information.',
+
+            '$blacklist [channel-name]' : 'block or unblock a channel from sending commands.' }],
+
+        ['''Other Commands''', {
+            '$donate' : 'view information about donations.',
+
+            'mbprefix <string>' : 'change the prefix from $. This command does not use a prefix!',
+
+            '$info' : 'get info on the bot.',
+
+            '$lang <name>' : 'change the language.',
+
+            '$clock [12]' : 'get the current time, optionally as 12 hour clock.' }
+        ]
+    ],
+
+    'web_foot_title' : 'Extra Information',
+    'web_foot' : 'Do not type the brackets when you type out the command! For example, mbprefix !, not mbprefix <!>',
+    'web_foot2' : 'Please join our Discord Support Server if you need more help:',
+
+    'about' : {'About' : ['Bot Created By: Jude Southworth', 'Github: <a href=https://github.com/JellyWX>https://github.com/JellyWX</a>', 'Want a bot making? Please contact me on Discord.', 'Hosting provider: OVH']},
+
+    'join' : 'Join our Discord Server',
+    'invite' : 'Invite the Bot',
+
+    'info' : '''
+Default prefix: `$`
+Reset prefix: `@{user} prefix $`
+Help: `{prefix}help`
+
+**Welcome to RemindMe!**
+Developer: <@203532103185465344>
+Cool guy who knows what he's on about: <@174243954487853056>
+Icon: <@253202252821430272>
+Find me on https://discord.gg/WQVaYmT and on https://github.com/JellyWX :)
+
+Framework: `discord.py`
+Hosting provider: OVH
+
+My other bot (Patron only):
+https://discordapp.com/oauth2/authorize?client_id=411224415863570434&scope=bot&permissions=35840
+
+*If you have enquiries about new features, please send to the discord server*
+*If you have enquiries about bot development for you or your server, please DM me*
+''',
+
+    'donate' : '''
+Thinking of adding a monthly contribution? Press below for my patreon and official bot server :D
+https://www.patreon.com/jellywx
+
+https://discord.gg/WQVaYmT
+
+Here's some more information:
+
+When you donate, Patreon will automatically rank you up on our Discord server, supposing you have properly linked your Patreon and Discord accounts!
+With your new rank, you'll be able to:
+: use Patron-only commands like `interval`
+: set more reminders (unlimited)
+: set longer reminders (2000 chars)
+: set more/longer tags
+
+Anyone who is a Patron, thank you :D You make this bot sustainable
+
+Please note, you must be connected to the Discord server to receive Patreon rewards.
+''',
+
+    'prefix' : {
+        'no_argument' : '''
+Please use this command as `mbprefix <prefix>`
+''',
+        'success' : '''
+Prefix changed to {prefix}
+''',
+
+        'too_long' : '''Please select a prefix under 5 characters'''
+    },
+
+    'timezone' : {
+
+        'no_argument' : '''
+Usage:
+    ```{prefix}timezone <Name>```
+Example:
+    ```{prefix}timezone Europe/London```
+All timezones: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+Current timezone: {timezone}''',
+
+        'no_timezone' : '''Timezone not recognized. A list is available at https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568''',
+
+        'success' : '''Timezone has been set to {timezone}. Your current time should be {time}'''
+    },
+
+    'restrict' : {
+
+        'disabled' : '''Disabled channel reminder permissions for roles.''',
+
+        'enabled' : '''Enabled channel reminder permissions for roles.''',
+
+        'allowed' : '''Allowed roles: {}'''
+    },
+
+    'clear' : {
+
+        'no_argument' : '''Please mention users you wish to remove messages of.'''
+
+    },
+
+    'remind' : {
+        'no_argument' : '''
+Usage:
+    ```{prefix}remind [channel mention or user mention] <time to or time at> <message>```
+Example:
+    ```{prefix}remind #general 10s Hello world```
+    ```{prefix}remind 10:30 It\'s now 10:30```''',
+
+        'invalid_tag' : '''Couldn't find a location by your tag present.''',
+
+        'invalid_time' : '''Make sure the time you have provided is in the format of [num][s/m/h/d][num][s/m/h/d] etc. or `day/month/year-hour:minute:second`.''',
+
+        'invalid_count' : '''Too many reminders in specified channel! Use `{prefix}del` to delete some of them, or use `{prefix}donate` to increase your maximum ($5 tier).''',
+
+        'invalid_chars' : '''Reminder message too long! (max 150, you used {}). Use `{prefix}donate` to increase your character limit to 1900 ($5 tier).''',
+
+        'invalid_chars_2000' : '''Discord restrictions mean we can\'t send reminders 2000+ characters. Sorry.''',
+
+        'no_perms' : '''You must have `Manage Messages` or have a role capable of sending reminders to that channel. Please talk to your server admin, and tell her/him to use the `{prefix}restrict` command to specify allowed roles.''',
+
+        'success' : '''New reminder registered for <{}{}> in {} seconds. If you want to delete this reminder, type `$del`.'''
+    },
+
+    'interval' : {
+        'no_argument' : '''
+Usage:
+    ```{prefix}interval [channel mention or user mention] <time to or time at> <interval> <message>```
+Example:
+    ```{prefix}interval #general 9:30 1d Good morning!```
+    ```{prefix}interval 0s 10s This will be really irritating```''',
+
+        'invalid_interval' : '''Make sure the interval you have provided is in the format of [num][s/m/h/d][num][s/m/h/d] etc. with no spaces, eg. 10s for 10 seconds or 10s12m15h1d for 10 seconds, 12 minutes, 15 hours and 1 day.''',
+
+        '8_seconds' : '''Please make sure your interval timer is longer than 8 seconds.''',
+
+        'donor' : '''You need to be a Patron (donating 2$ or more) to access this command! Type `{prefix}donate` to find out more.''',
+
+        'success' : '''New interval registered for <{}{}> in {} seconds . You can\'t edit the reminder now, so you are free to delete the message.''',
+
+        'removed' : '''There appears to be no patrons on your server, so the interval has been removed.'''
+
+    },
+
+    'autoclear' : {
+        'disable' : '''Autoclearing disabled on {}''',
+
+        'enable' : '''{} second autoclearing enabled.''',
+    },
+
+    'del' : {
+        'listing' : '''Listing reminders on this server... (there may be a small delay, please wait for the "List (1,2,3...)" message).''',
+
+        'listed' : '''List (1,2,3...) the reminders you wish to delete, or type anything else to cancel.''',
+
+        'count' : '''Deleted {} reminders!'''
+    },
+
+    'todo' : {
+        'server_only' : '''Please use `{prefix}todo` for your personal TODO list. `{prefix}todos` is only for server use.''',
+
+        'add' : '''*Do `{prefix}{command} add <message>` to add an item to your TODO, or type `{prefix}{command} help` for more commands!*''',
+
+        'too_long' : '''Sorry, but TODO message sizes are limited to 80 characters. Keep it concise :)''',
+
+        'too_long2' : '''Sorry, but TODO lists are capped at 800 characters. Maybe, get some things done?''',
+
+        'added' : '''Added \'{name}\' to todo!''',
+
+        'removed' : '''Removed \'{}\' from todo!''',
+
+        'error_value' : '''Removal item must be a number. View the numbered TODOs using `{prefix}{command}`''',
+
+        'error_index' : '''Couldn\'t find item by that number. Are you in the correct todo list?''',
+
+        'help' : '''To use the TODO commands, do `{prefix}{command} add <message>`, `{prefix}{command} remove <number>`, `{prefix}{command} clear` and `{prefix}{command}` to add to, remove from, clear or view your todo list.''',
+
+        'cleared' : '''Cleared todo list!'''
+    },
+
+    'tags' : {
+
+        'deleted' : '''Deleted tag {}''',
+
+        'added' : '''Added tag {}''',
+
+        'invalid_count' : '''Sorry, but for normal users tags are capped at 6 tags. Please remove some or consider donating with `{prefix}donate` ($5 tier).''',
+
+        'invalid_chars' : '''Tags are capped at 80 characters. Keep it concise!''',
+
+        'colon' : '''Please add a colon to split the name of the tag from the body.''',
+
+        'illegal' : '''Please don\'t use keywords `add, new, remove, del` in the names of your tags.''',
+
+        'unfound' : '''Couldn\'t find the tag by the name you specified.''',
+
+        'help' : '''Use `{prefix}tag add <name>: <message>` to add new tags. Use `{prefix}tag remove <name>` to delete a tag. Use `{prefix}tag <name>` to view a tag. Use `{prefix}tag` to list all tags'''
+    },
+
+    'blacklist' : {
+        'removed_from' : '''Removed blacklists from specified channels.''',
+
+        'added_from' : '''Added specified channels to blacklist.''',
+
+        'removed' : '''Removed current channel from blacklist.''',
+
+        'added' : '''Added current channel to blacklist.'''
+
+    },
+
+    'lang' : {
+
+        'invalid' : '''Languages:
+{}''',
+
+        'set' : '''Language set to English.''',
+    },
+
+    'clock' : {
+        'time' : '''Current time is {}.''',
+    }
+
+}
---
+++ strings_ES.py
@@ -0,0 +1,262 @@
+#: español
+
+{
+    'blacklisted' : ''':x: Este canal está en la blacklist :x:''',
+
+    'admin_required' : 'Necesitas permisos de administrador para usar este comando.',
+
+    'help' : '''Por favor visita https://jellywx.co.uk/help?lang=ES''',
+
+    'help_raw' : [
+['Comandos de recordatorios', {
+'$del' : 'Elimina recordatorios e intervalos en tu servidor.',
+
+'$remind [usuario/canal] <tiempo-para-recordatorio> <mensaje>' : 'coloca un recordatorio. El tiempo debe ponerse como [num][s/m/h/d], por ejemplo 10s para 10 segundos o 2s10m para 2 segundos 10 minutos. Un tiempo exacto puede ser colocado como `día`/`mes`/`año`-`hora`:`minuto`:`segundo`.',
+
+'$interval [usuario/canal] <tiempo-para-recordatorio> <intervalo> <mensaje>' : 'coloca un intervalo, donde el `mensaje` se enviará cada cierto `intervalo` iniciando desde el `tiempo-para-recordatorio` dado. El tiempo se coloca como dice arriba. Ej. `$interval 0s 20m ¡Hola Mundo!` enviará `¡Hola Mundo!` a tu canal cada 20 minutos.',
+
+'$todo' : 'comandos relacionado a la lista TO-DO (lista de tareas). Utiliza `$todo help` para más información.',
+
+'$todos' : 'lo mismo que `$todo` pero para tareas a nivel de servidor.',
+
+'$timezone' : 'configura la zona horaria del servidor, para facilitar los recordatorios basados en tiempo.'
+}],
+
+['Comandos de administración', {
+'$autoclear [veces] [canales]' : 'activa/desactiva la limpieza automática de mensajes, donde los mensajes enviados al canal (por defecto el canal en el que estás) serán eliminados automáticamente luego de un tiempo (por defecto 10 segundos).',
+
+'$clear <usuarios>' : 'elimina mensajes enviados por los usuarios mencionados.',
+
+'$restrict [roles]' : 'añade/remueve permiso a ciertos roles mencionados para colocar recordatorios e intervalos.',
+
+'$tag' : 'comando para alias. Utiliza `$tag help` para más información.',
+
+'$blacklist [canal]' : 'bloquea o desbloquea un canal de enviar comandos.'
+}],
+
+['Otros comandos', {
+'$donate' : 'muestra información sobre donaciones.',
+
+'mbprefix <prefijo>' : 'cambia el prefijo "$". ¡Este comando no utiliza un prefijo!',
+
+'$info' : 'obtén información sobre el bot.',
+
+'$lang <nombre>' : 'cambia el idioma.',
+
+'$clock' : 'obtén la hora actual.'}]
+],
+
+    'web_foot_title' : 'Información extra',
+    'web_foot' : '¡No coloques los paréntesis al escribir un comando! Por ejemplo, `mbprefix !`, no `mbprefix <!>`',
+    'web_foot2' : 'Por favor dirígete a nuestro servidor de Discord si necesitas más ayuda',
+
+    'about' : {'Sobre el bot' : ['Creado por: Jude Southworth', 'Github: <a href=https://github.com/JellyWX>https://github.com/JellyWX</a>', 'Want a bot making? Please contact me on Discord.', 'Hosting provider: OVH']},
+
+    'join' : 'Ingresa a nuestro servidor de Discord',
+    'invite' : 'Invita el bot a tu servidor',
+
+    'info' : '''
+Prefijo por defecto: `$`
+Prefijo seleccionado: `@{user} prefix $`
+Ayuda: `{prefix}help`
+
+**Bienvenido a RemindMe!**
+Desarrollador: <@203532103185465344>
+Un chico cool: <@174243954487853056>
+Ícono: <@253202252821430272>
+Encuéntrame en https://discord.gg/WQVaYmT y en https://github.com/JellyWX :)
+
+Framework: `discord.py`
+Proveedor de host: OVH
+
+Mi otro bot (solo Patreon):
+https://discordapp.com/oauth2/authorize?client_id=411224415863570434&scope=bot&permissions=35840
+
+*Si tienes consultas sobre nuevas caracterísicas, por favor envíalas a nuestro servidor de Discord*
+*Si tienes consultas sobre desarrollo de bot para tí y tu servidor, por favor envíame un mensaje directo*
+''',
+
+    'donate' : '''
+¿Pensando en aportar una contribución mensual? Presiona abajo para mi Patreon y el servidor oficial del bot :D
+https://www.patreon.com/jellywx
+
+https://discord.gg/WQVaYmT
+
+Aquí hay un poco más información:
+
+Cuando donas, Patreon automáticamente te dará el rango en nuestro servidor de Discord, siempre y cuando hayas vinculado tu Patreon con Discord.
+Con tu nuevo rango, podrás:
+: usar comandos exclusivos como `interval`
+: colocar más recordatorios (ilimitados)
+: colocar recordatorios más largos (2000 caracteres)
+: seleccionar más/largos tags
+
+Para quienes estén en Patreon, muchas gracias :D Haces que este bot pueda sostenerse.
+
+Ten en cuenta que debes estar conectado a nuestro servidor de Discord para recibir las recompenzas de Patreon.
+''',
+
+    'prefix' : {
+        'no_argument' : '''
+Por favor utiliza este comando como `mbprefix <prefijo>`
+''',
+        'success' : '''
+Prefijo cambiado a {prefix}
+''',
+
+        'too_long' : '''Please select a prefix under 5 characters'''
+    },
+
+    'timezone' : {
+
+        'no_argument' : '''
+Uso:
+    ```{prefix}timezone <nombre>```
+Example:
+    ```{prefix}timezone Europe/London```
+Todas las zonas horarias: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+Zona horaria actual: {timezone}''',
+
+        'no_timezone' : '''Zona horaria no reconocida. Una lista está disponible en https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568''',
+
+        'success' : '''La zona horaria ha sido cambiada a {timezone}. Tu hora actual debería ser {time}'''
+    },
+
+    'restrict' : {
+
+        'disabled' : '''Desactivados los permisos de recordatorios para el rol.''',
+
+        'enabled' : '''Activados los permisos de recordatorios para el rol.''',
+
+        'allowed' : '''Roles permitidos: {}'''
+    },
+
+    'clear' : {
+
+        'no_argument' : '''Por favor menciona los usuarios de los que deseas eliminar los mensajes.'''
+
+    },
+
+    'remind' : {
+        'no_argument' : '''
+Uso:
+    ```{prefix}remind [canal o usuario] <tiempo para/tiempo exacto> <mensaje>```
+Ejemplo:
+    ```{prefix}remind #general 10s Hola mundo```
+    ```{prefix}remind 10:30 Son las 10:30```''',
+
+        'invalid_tag' : '''No es posible encontrar la localización de tu tag.''',
+
+        'invalid_time' : '''Asegúrate que la hora que colocaste es en el formato [num][s/m/h/d][num][s/m/h/d] etc. o `día/mes/año-hora:minuto:segundo`.''',
+
+        'invalid_count' : '''¡Demasiados recordatorios en el canal especificado! Utiliza `{prefix}del` para eliminar algunos, o utiliza `{prefix}donate` para aumentar el límite (tier de $5).''',
+
+        'invalid_chars' : '''¡Recordatorio demasiado largo! (máx. 150, usaste {}). Utiliza `{prefix}donate` para aumentar el límite a 1900 caracteres (tier de $5).''',
+
+        'invalid_chars_2000' : '''Discord no permite colocar recordatorios con 2000+ caracteres. Lo siento,''',
+
+        'no_perms' : '''Necesitas `Administrar Mensajes` tener un rol con el cual puedas colocar recordaorios a ese canal. Contacta al admnistrador de tu servidor y dile que utilice el comando `{prefix}restrict` para especificar roles permitidos.''',
+
+        'success' : '''Nuevo recordatorio registrado para <{}{}> en {} segundos. Ya no puedes editarlo, así que eres libre de eliminarlo.'''
+    },
+
+    'interval' : {
+        'no_argument' : '''
+Uso:
+    ```{prefix}interval [canal o usuario] <tiempo para/tiempo exacto> <intervalo> <mensaje>```
+Ejemplo:
+    ```{prefix}interval #general 9:30 1d ¡Buen día!```
+    ```{prefix}interval 0s 10s Esto será muy irritante```''',
+
+        'invalid_interval' : '''Asegúrate que la hora que colocaste es en el formato [num][s/m/h/d][num][s/m/h/d] etc. sin espacios, ej. 10s para 10 segundos o 10s12m15h1d for 10 segundos, 12 minutos, 15 horas y 1 día.''',
+
+        '8_seconds' : '''Por favor asegúrate que tu intervalo es mayor a 8 segundos.''',
+
+        'donor' : '''¡Necesitas ser un Patreon (dona 2$ o más) para acceder a este comando! Escribe `{prefix}donate` para más información.''',
+
+        'success' : '''Nuevo intervalo registrado para <{}{}> en {} segundos . Ya no puedes editarlo, así que eres libre de eliminarlo.''',
+
+        'removed' : '''Parece que no hay Patreons en tu servidor, por lo tanto el intervalo se ha eliminado.'''
+
+    },
+
+    'autoclear' : {
+        'disable' : '''Autolimpieza desactivada en {}''',
+
+        'enable' : '''Autolimpieza de {} segundos activada.''',
+    },
+
+    'del' : {
+        'listing' : '''Listando recordatorios en este servidor... (puede haber un pequeño retraso, espera que el mensaje "Selecciona (1,2,3...)" aparezca).''',
+
+        'listed' : '''Selecciona (1,2,3...) el recordaorio que quieres eliminar, o escribe cualquier otra cosa para cancelar.''',
+
+        'count' : '''¡Se han eliminado {} recordatorios!'''
+    },
+
+    'todo' : {
+        'server_only' : '''Por favor utiliza `{prefix}todo` para tu lista de tareas personal. `{prefix}todos` es para las tareas del servidor entero.''',
+
+        'add' : '''*Pon `{prefix}{command} add <mensaje>` para añadir un ítem a tu TODO, o escribe `{prefix}{command} help` para más comandos*''',
+
+        'too_long' : '''Disculpa, pero los ítems están limitados a 80 caracteres. Mantenlo conciso :)''',
+
+        'too_long2' : '''Disculpa, pero la lista no puede excederse de los 800 caracteres. ¿Tal vez completar algunas tareas?''',
+
+        'added' : '''¡Añadido \'{name}\' a la lista de tareas!''',
+
+        'removed' : '''¡Eliminado \'{}\' de la lista de tareas!''',
+
+        'error_value' : '''El ítem a remover ha de ser un número. Mira el número de las tareas usando `{prefix}{command}`''',
+
+        'error_index' : '''No se pudo encontrar un ítem con ese número. ¿Estás en la lista correcta?''',
+
+        'help' : '''Para usar comandos de la lista de tareas (TODO), pon `{prefix}{command} add <mensaje>`, `{prefix}{command} remove <número>`, `{prefix}{command} clear` y `{prefix}{command}` para añadir, remover, limpiar o mirar tu lista de tareas.''',
+
+        'cleared' : '''¡Limpiada la lista de tareas!'''
+    },
+
+    'tags' : {
+
+        'deleted' : '''Eliminado el tag {}''',
+
+        'added' : '''Añadido el tag {}''',
+
+        'invalid_count' : '''Lo siento, para usuarios normales hay un límite de 6 tags. Por favor elimina algunos o considera donar con `{prefix}donate` (tier de 5$).''',
+
+        'invalid_chars' : '''Los tags están limitados a 80 caracteres. ¡Mantenlo conciso!''',
+
+        'colon' : '''Por favor añade dos puntos para separar el nombre del tag con el cuerpo.''',
+
+        'illegal' : '''Por favor no utilices palabras claves `add, new, remove, del` en el nombre de tus tags.''',
+
+        'unfound' : '''No se pudo encontrar el tag con el nombre que has especificado.''',
+
+        'help' : '''Utiliza `{prefix}tag add <nombre>: <mensaje>` para añadir nuevos tags. Utiliza `{prefix}tag remove <nombre>` para eliminar un tag. Utiliza `{prefix}tag <nombre>` para ver un tag. Utiliza `{prefix}tag` para ver todos los tags.'''
+    },
+
+    'blacklist' : {
+        'removed_from' : '''Removidas las blacklists de los canales especificados.''',
+
+        'added_from' : '''Añadido los canales especificados a la blacklist.''',
+
+        'removed' : '''Removido el canal actual de la blacklist.''',
+
+        'added' : '''Añadido el canal actual a la blacklist.'''
+
+    },
+
+    'lang' : {
+
+        'invalid' : '''Idiomas:
+{}''',
+
+        'set' : '''Idioma cambiado a Español.''',
+    },
+
+    'clock' : {
+
+        'time' : '''La hora actual es {}.''',
+    }
+
+}
