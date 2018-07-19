9ff8f71757ee5bf9e63e9d167f6a952161373c84
胡祥又 <25381735+huxiangyou@users.noreply.github.com>
2018-07-19 22:56:46 +0800

    Translated strings and changed some translations

--- strings_ZH.py
+++ strings_ZH.py
@@ -15,7 +15,7 @@
 
             '$remind [用户/频道] <提醒时间> <内容>' : '设置一个提醒。将时间设置成[数字][s/m/h/d]的格式，比如 10s 即10秒， 2s10m 即10分钟2秒。也可用 `日`/`月`/`年`-`时`:`分`:`秒` 的格式设置精确时间。',
 
-            '$interval [用户/频道] <提醒时间> <重复周期> <内容>' : '设置一个周期重复提醒，给出的`内容`将从`提醒时间`开始，每隔`重复周期`被重复提醒一次。将时间设置成如上格式。例如`$interval 0s 20m Hello World!`就会每20分钟向该频道发送一次`Hello World!`。',
+            '$interval [用户/频道] <提醒时间> <重复周期> <内容>' : '设置一个周期重复提醒，给出的`内容`将从`提醒时间`开始，每隔`重复周期`被重复提醒一次。将时间设置成上述格式。例如`$interval 0s 20m Hello World!`就会每20分钟向该频道发送一次`Hello World!`。',
 
             '$todo' : '与待办事项列表有关的指令。使用`$todo help`来获取更多信息。',
 
@@ -24,7 +24,7 @@
             '$timezone' : '设置服务器的时区，从而更容易地设置提醒。' }],
 
         ['''管理指令''', {
-            '$autoclear [时间/秒] [频道]' : '开启/关闭自动清除功能，在指定频道（默认为该频道）发送的信息将在发出后指定时间（默认10秒）被自动清除。',
+            '$autoclear [时间/秒] [频道]' : '开启/关闭自动清除功能。在指定频道（默认为该频道）发送的信息将在发出后指定时间（默认10秒）被自动清除。',
 
             '$clear <指定用户>' : '清除指定用户发送的信息。',
 
@@ -48,7 +48,7 @@
     ],
 
     'web_foot_title' : '更多信息',
-    'web_foot' : '在输入指令时不要输入括号！比如， mbprefix ! ，而不是 mbprefix <!>',
+    'web_foot' : '在输入指令时不要输入括号！比如，要输入 mbprefix ! ，而不是 mbprefix <!>',
     'web_foot2' : '如果你需要更多帮助，请加入我们的Discord服务器：',
 
     'about' : {'关于' : ['bot制作者：Jude Southworth', 'Github：<a href=https://github.com/JellyWX>https://github.com/JellyWX</a>', '想要制作bot？请在Discord上联系我。', '程序架构：OVH']},
@@ -65,7 +65,7 @@
 开发者：<@203532103185465344>
 这个人超屌的：<@174243954487853056>
 图标：<@253202252821430272>
-在 https://discord.gg/WQVaYmT 或 https://github.com/JellyWX 可以找到我 :)
+可以在 https://discord.gg/WQVaYmT 或 https://github.com/JellyWX 找到我 :)
 
 程序架构：`discord.py`
 主机提供：OVH
@@ -85,16 +85,16 @@ https://discord.gg/WQVaYmT
 
 更多信息：
 
-如果你将你的Patreon和Discord账号连接，当你捐赠的时候，Patreon会自动将你列在我们的Discord服务器的排名上！
-有了这个新的排名，你就可以：
+如果你将你的Patreon和Discord账号连接，当你捐赠的时候，Patreon会自动将你从我们的Discord服务器上标识出来！
+当你被标识出来后，你就可以：
 : 使用仅限Patron的指令，如`interval`
 : 设置更多提醒事项（无限制）
 : 设置更长的提醒事项（2000字符）
 : 设置更多/更长的标签
 
-感谢所有已经成为Patron的人 :D 是你们让这个bot可以继续维护下去。
+感谢所有已经成为Patron的人 :D 是你们让这个bot得以维护。
 
-请注意，务必将你的Patreon和Discord账号连接，这样才能获得Patreon的特权。
+请注意，务必将你的Patreon和Discord账号连接，这样才能得到Patreon给出的权限。
 ''',
 
     'prefix' : {
@@ -120,18 +120,18 @@ https://discord.gg/WQVaYmT
 
         'no_timezone' : '''未知时区。可用的时区列表在 https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568''',
 
-        'success' : '''成功设置时区为{timezone}。您当前时间应为{time}'''
+        'success' : '''时区已被设置为{timezone}。您当前的时间应为{time}'''
     },
 
     'restrict' : {
 
-        'disabled' : '''成功禁止该身份组使用频道提醒。''',
+        'disabled' : '''成功禁止该身份组设置频道提醒。''',
 
-        'enabled' : '''成功允许该身份组使用频道提醒。''',
+        'enabled' : '''成功允许该身份组设置频道提醒。''',
 
-        'allowed' : '''允许使用指令的身份组：{}''',
+        'allowed' : '''允许设置提醒的身份组：{}''',
 
-        'help' : '''请指定身份组来更改其提醒事项指令的使用权限。'''
+        'help' : '''请指定身份组来更改其使用提醒指令的权限。'''
     },
 
     'clear' : {
@@ -150,21 +150,49 @@ https://discord.gg/WQVaYmT
 
         'invalid_tag' : '''无法找到你给出的发送对象。''',
 
-        'invalid_time' : '''确保你给出的时间是以 [数字][s/m/h/d][数字][s/m/h/d]... 的格式，或 `日/月/年-时:分:秒` 的格式。
+        'invalid_time' : '''请以 [数字][s/m/h/d][数字][s/m/h/d]... 的格式，或 `日/月/年-时:分:秒` 的格式给出时间。
 
 给出的时间不能超过未来50年。''',
 
         'invalid_count' : '''这个频道里的待提醒事项太多了！用`{prefix}del`来删掉一些，或者用`{prefix}donate`来扩大最大量（5美元档）''',
 
-        'invalid_chars' : '''提醒内容太长了！（最大150字符，你输入了{}字符）。用`{prefix}donate`来增加最大长度到1900字符（5美元档）。''',
+        'invalid_chars' : '''提醒内容太长了！（最大150字符，你输入了{}字符）。用`{prefix}donate`来把最大长度扩大到1900字符（5美元档）。''',
 
         'invalid_chars_2000' : '''Discord限制一条信息不能超过2000字符。抱歉。''',
 
-        'no_perms' : '''你必须拥有`管理信息`权限或可以向该频道发送提醒事项的身份组。请联系服务器的管理员，让他使用`{prefix}restrict`指令来指定允许使用指令的身份组。''',
+        'no_perms' : '''你必须拥有`管理信息`权限或可以向该频道发送提醒的身份组。请联系服务器的管理员，让他使用`{prefix}restrict`指令来指定允许设置提醒的身份组。''',
 
-        'success' : '''成功给 <{}{}> 设置{}秒钟后的提醒。如果你想删除这条提醒，输入`$del`。''',
+        'success' : '''发往<{}{}>的提醒将在{}秒后发出。如果你想删除这条提醒，输入`$del`。''',
+    },
 
-        'success_new' : '''成功给 {} 设置{}秒钟后的提醒。如果你想删除这条提醒，输入`$del`。'''
+    'natural' : {
+        'no_argument' : '''
+**新** 自然语言处理
+举例：
+    ```{prefix}natural in 10 minutes send Hello World! to #general```
+    ```{prefix}natural at 18:00 send 搞一波大操作！```
+    ```{prefix}natural on the 16th of july at 14:00 send 该交订阅费了！ to #subs```
+    ```{prefix}natural now send 10分钟过去了！ every 10 minutes to #timer```
+关键词：
+    `send`：给出提醒内容
+    `every`：给出重复周期
+    `to`：给出发往的对象
+用法：
+    ```{prefix}natural <时间短语> send <内容> [every 重复周期短语] [to #频道/@用户]```
+    ''',
+
+        'success' : '''发给 {} 的提醒将在{}秒后发出。如果你想删除这条提醒，输入`$del`。''',
+
+        'bad_time' : '''未能成功处理给出的时间。请尽可能清楚地表述，比如`16th of july`或是`in 20 minutes`''',
+
+        'long_time' : '''时间太长了！不要让提醒的时间超过未来50年''',
+
+        'send' : ''' send ''',
+
+        'to' : ''' to ''',
+
+        'every' : ''' every '''
+
     },
 
     'interval' : {
@@ -175,7 +203,7 @@ https://discord.gg/WQVaYmT
     ```{prefix}interval #general 9:30 1d 早上好！```
     ```{prefix}interval 0s 10s 这能烦死人```''',
 
-        'invalid_interval' : '''确保你给出的重复周期是以[num][s/m/h/d][num][s/m/h/d]...的格式，不要空格，比如 10s 即10秒，或 10s12m15h1d 即1天15小时12分钟10秒。
+        'invalid_interval' : '''请以[num][s/m/h/d][num][s/m/h/d]...的格式给出重复周期，不要空格，比如 10s 即10秒，或 10s12m15h1d 即1天15小时12分钟10秒。
 
 给出的时间不能超过未来50年。''',
 
@@ -183,24 +211,24 @@ https://discord.gg/WQVaYmT
 
         'donor' : '''你必须成为Patron（捐赠2美元或更多）才能使用这个指令！输入`{prefix}donate`来了解更多。''',
 
-        'success' : '''成功给<{}{}>设置在{}秒钟后开始的周期重复提醒。你还不能编辑提醒的内容，但你可以删除它。''',
+        'success' : '''发给<{}{}>的周期重复提醒将在{}秒后开始。你还不能编辑提醒的内容，但你可以删除它。''',
 
         'removed' : '''这个服务器上现在没有Patron了，所以设置的周期重复提醒已被删除。'''
 
     },
 
     'autoclear' : {
-        'disable' : '''成功取消 {} 的自动清除。''',
+        'disable' : '''关闭自动清除 {} 的信息。''',
 
-        'enable' : '''成功开启 {} 的自动清除。''',
+        'enable' : '''开启自动清除 {} 的信息。''',
     },
 
     'del' : {
         'listing' : '''正在列出此服务器上的待提醒事项。（可能会有延迟，请等待"列出 (1,2,3...)"这条信息被发出。）''',
 
-        'listed' : '''列出你想要删除的提醒事项的编号(1,2,3...)，或者发送随便其他的内容来取消。''',
+        'listed' : '''列出事项的编号(1,2,3...)来删除提醒，或者发送任意其他内容来取消。''',
 
-        'count' : '''成功删除{}条提醒！'''
+        'count' : '''删除了{}条提醒！'''
     },
 
     'todo' : {
@@ -210,17 +238,17 @@ https://discord.gg/WQVaYmT
 
         'too_long' : '''抱歉，待办事项的内容限制在80字符以下。简短些 :)''',
 
-        'too_long2' : '''抱歉，待办事项列表一共不能超过800字符。或许可以先去完成其中的一些？''',
+        'too_long2' : '''抱歉，待办事项列表总计不能超过800字符。或许可以先去完成一些？''',
 
-        'added' : '''成功向待办事项列表中加入“{name}”！''',
+        'added' : '''成功将“{name}”加入待办事项列表！''',
 
-        'removed' : '''成功从待办事项列表中移除“{name}”！''',
+        'removed' : '''“{name}”已从待办事项列表中移除！''',
 
         'error_value' : '''输入待移除事项的编号。用`{prefix}{command}`来查看待办事项列表。''',
 
         'error_index' : '''无法找到该数字对应的事项。你不会是迷路了吧？''',
 
-        'help' : '''使用待办事项列表，用`{prefix}{command} add <内容>`来增加待办事项，用`{prefix}{command} remove <编号>`来移除待办事项，用`{prefix}{command} clear`来清空待办事项列表，以及用`{prefix}{command}`来查看待办事项列表。''',
+        'help' : '''使用待办事项列表，用`{prefix}{command} add <内容>`来增加待办事项，用`{prefix}{command} remove <编号>`来移除待办事项，用`{prefix}{command} clear`来清空待办事项列表，用`{prefix}{command}`来查看待办事项列表。''',
 
         'cleared' : '''已清空待办事项列表！'''
     },
@@ -264,7 +292,7 @@ https://discord.gg/WQVaYmT
     },
 
     'clock' : {
-        'clock' : '''现在时间为 {}。''',
+        'clock' : '''当前时间为 {}。''',
     }
 
 }
